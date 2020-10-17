fcea5f103c8ad1cd14688843d7cbd9a75233b6cf
Mehmet <mhmtsytrk@gmail.com>
2020-10-17 18:52:24 +0300

    Coupling in Java

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,10 +1,17 @@
+import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
         FullTimeMentor fullTime=new FullTimeMentor();
         MentorAccount mentor=new MentorAccount(fullTime);   // Poly way
         mentor.manageAccount();
+
+        PartTimeMentor partTime=new PartTimeMentor();
+        MentorAccount mentor1=new MentorAccount(partTime);
+        mentor1.manageAccount();
+
     }
 }
