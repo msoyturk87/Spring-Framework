9f0d50087b11553f1aef96613a0e6fe510d0a387
Mehmet <mhmtsytrk@gmail.com>
2020-10-17 18:36:26 +0300

    Loose Coupled

---
+++ Spring-00-LooselyCoupled/src/service/PartTimeMentor.java
@@ -0,0 +1,9 @@
+package service;
+
+import implementation.Mentor;
+
+public class PartTimeMentor implements Mentor {
+    @Override
+    public void createAccount() {
+        System.out.println("Part Time Mentor Account is created... ");    }
+}
