24482f4fb1f0e7132ac948bf979db99777f0e423
Mehmet <mhmtsytrk@gmail.com>
2020-10-17 20:43:21 +0300

    with Maven

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -5,6 +5,7 @@ import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
+
         FullTimeMentor fullTime=new FullTimeMentor();
         MentorAccount mentor=new MentorAccount(fullTime);   // Poly way
         mentor.manageAccount();
