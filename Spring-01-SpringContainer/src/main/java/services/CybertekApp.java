a9b8e757df6f8f006ac9df7805f9b12022e333ad
Mehmet <mhmtsytrk@gmail.com>
2020-10-17 21:46:56 +0300

    with Bean

--- Spring-01-SpringContainer/src/main/java/services/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/services/CybertekApp.java
@@ -10,11 +10,15 @@ public class CybertekApp {
         //BeanFactory container=new ClassPathXmlApplicationContext("config.xml");
         ApplicationContext container=new ClassPathXmlApplicationContext("config.xml");
 
+        //Way 1
         Mentor mentor=(Mentor)container.getBean("fullTimeMentor");
         mentor.createAccount();
-
+        //Way 2
         Mentor mentors=(Mentor)container.getBean("partTimeMentor");
         mentors.createAccount();
+        // Way 3
+        Mentor m=container.getBean("fullTimeMentor",Mentor.class);
+        m.createAccount();
 
     }
 }
