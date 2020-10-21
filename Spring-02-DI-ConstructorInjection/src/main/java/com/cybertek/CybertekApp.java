ae5ef2a03c93460b1833199ba83c0583f74d00d9
Mehmet <mhmtsytrk@gmail.com>
2020-10-21 03:34:56 +0300

    DI-Setter Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,21 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.services.Java;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = (Course) container.getBean("java");
+        course.getTeachingHours();
+
+
+
+    }
+
+}
