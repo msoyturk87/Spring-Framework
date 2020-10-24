a73551612aa82f60e606b1ff50e4ad18770a2aa4
Mehmet <mhmtsytrk@gmail.com>
2020-10-24 21:43:22 +0300

    Java Code -  Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,18 +1,27 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-@Component
 public class Java implements Course {
 
+    private ExtraSessions extraSessions;
+
+    @Autowired
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching Java hours 30");
+        System.out.println("Weekly teaching Java hours "+ (30+extraSessions.getHours()));
     }
+
+
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Selenium.java
@@ -3,7 +3,6 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Selenium implements Course {
     @Override
     public void getTeachingHours() {
