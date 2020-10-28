a8763d94b77f24b79ceea800c975444887f6a3f5
Mehmet <mhmtsytrk@gmail.com>
2020-10-28 04:10:17 +0300

    SpringBoot Demo

--- Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/interfaces/Java.java
+++ Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/interfaces/Java.java
@@ -1,13 +1,26 @@
 package com.cybertek.interfaces;
 
 import com.cybertek.services.Course;
+import com.cybertek.services.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    private ExtraSessions extraSessions;
+
+    @Value("${instructor}")
+    private String instructorName;
+
     @Override
     public int getTeachingHours() {
-        return 20;
+        return 20+extraSessions.getHours();
+    }
+    @Autowired  // optional defauşt also gives same result
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
     }
 }
---
+++ Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/interfaces/OfficeHours.java
@@ -0,0 +1,14 @@
+package com.cybertek.interfaces;
+
+import com.cybertek.services.ExtraSessions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours implements ExtraSessions {
+
+
+    @Override
+    public int getHours() {
+        return 4;
+    }
+}
