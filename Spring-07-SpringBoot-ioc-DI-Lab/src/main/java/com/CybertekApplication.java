9b8bdec6c5a1cbc41ec624bb26fcfb1100bc29ca
Mehmet <mhmtsytrk@gmail.com>
2020-12-12 21:23:03 +0300

    SQL Reviews

---
+++ Spring-07-SpringBoot-ioc-DI-Lab/src/main/java/com/CybertekApplication.java
@@ -0,0 +1,23 @@
+package com;
+
+import com.calculator.Calculator;
+import com.enums.City;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CybertekApplication {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext container=SpringApplication.run(CybertekApplication.class, args);
+
+		Calculator calculator=container.getBean("calculator",Calculator.class);
+
+		System.out.println(calculator.getTotalCarpetCost(City.ARLINGTON));
+
+
+	}
+
+
+}
