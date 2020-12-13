269a12dcd61f178937156d4fe836456449456e8a
Mehmet <mhmtsytrk@gmail.com>
2020-12-14 01:56:03 +0300

    ORM-TABLE CREATION

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,11 +1,18 @@
 package com.cybertek.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.ValueGenerationType;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "cars")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Car {
 
     @Id
@@ -14,6 +21,9 @@ public class Car {
 
     private String make;
     private String  model;
-
-
+    // id generated from GeneratedValue so we nedd constr without ID
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
 }
