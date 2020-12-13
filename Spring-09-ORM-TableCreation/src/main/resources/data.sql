3950a97ce4732a882772e2ce9bd5731fed9f5b30
Mehmet <mhmtsytrk@gmail.com>
2020-12-14 00:44:15 +0300

    ORM-TABLE CREATION

---
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student(first_name,last_name,email) VALUES('Mike','Smith','mike@cybertek.com');
+INSERT INTO student(first_name,last_name,email) VALUES('Adam','Shaun','adam@cybertek.com');
+INSERT INTO student(first_name,last_name,email) VALUES('Tom','Hanks','tom@cybertek.com');
+INSERT INTO student(first_name,last_name,email) VALUES('Brad','Kelly','brad@cybertek.com');
