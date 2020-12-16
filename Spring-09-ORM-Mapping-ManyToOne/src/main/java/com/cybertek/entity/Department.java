8e15dfffdf70ea55dbe8d0c4250b30c11416ed14
Mehmet <mhmtsytrk@gmail.com>
2020-12-16 05:13:19 +0300

    ORM-Mapping- OneToOne and OneToMany

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Department.java
@@ -0,0 +1,26 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="departments")
+public class Department extends BaseEntity {
+
+    private String department;
+    private String division;
+
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,10 +1,8 @@
 package com.cybertek.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.ValueGenerationType;
 
 import javax.persistence.*;
 
