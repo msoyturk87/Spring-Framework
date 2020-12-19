caa3ac13fbcafe171609d6b5ba87eafe33c762d0
Mehmet <mhmtsytrk@gmail.com>
2020-12-19 13:27:07 +0300

    ORM-Mapping- ManyToMany

--- Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -1,21 +1,36 @@
 package com.cybertek.entity;
 
-import org.w3c.dom.stylesheets.LinkStyle;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String firstName;
     private String lastName;
 
-    @OneToMany (mappedBy = "person")
-    private List<Address> address;
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;
 
 
+    //case-1
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name="person_id")
+//    private List<Address> addresses;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
