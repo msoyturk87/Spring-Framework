ca94de415314c421996206dff06a3982e424a177
Mehmet <mhmtsytrk@gmail.com>
2020-12-24 04:06:55 +0300

    ORM-JPQL Named Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -83,4 +83,10 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     @Query("SELECT e FROM Employee e ORDER BY e.salary DESC ")
     List<Employee> getEmployeeBySalaryOrderByDesc();
 
+
+    //Native Query
+    @Query(value = "SELECT * FROM employees WHERE salary = ?1",nativeQuery = true )
+    List<Employee> readEmployeeBySalary(int salary);
+
+
 }
