9ef175de49190df6f300dc02714beb9b105b0ecd
Mehmet <mhmtsytrk@gmail.com>
2021-01-16 22:04:42 +0300

    Security-HttpBasic-Database

--- Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -1,44 +1,44 @@
 package com.cybertek.bootstrap;
 
 import com.cybertek.entity.User;
-import com.repository.UserRepository;
+import com.cybertek.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class DataGenerator {
+@Service
+public class DataGenerator implements CommandLineRunner {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
     public DataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public void run(String... args) throws Exception{
-
+    @Override
+    public void run(String... args) throws Exception {
 
         //Delete All
         this.userRepository.deleteAll();
 
         //Create users
-        User employee=new User("ozzy", passwordEncoder.encode("ozzy123"),"USER","");
-        User admin=new User("admin",passwordEncoder.encode("ozzy123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
-        User manager=new User("manager",passwordEncoder.encode("ozzy123"),"MANAGER","ACCESS_TEST1");
+        User employee = new User("ozzy", passwordEncoder.encode("ozzy123"), "USER","");
+        User admin = new User("admin", passwordEncoder.encode("admin123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
+        User manager = new User("manager", passwordEncoder.encode("manager123"),"MANAGER","ACCESS_TEST1");
 
-        List<User> users= Arrays.asList(employee,admin,manager);
+        List<User> users = Arrays.asList(employee,admin,manager);
+
         userRepository.saveAll(users);
 
+
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
 
-        return new BCryptPasswordEncoder();
-    }
 }
--- Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/entity/User.java
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/entity/User.java
@@ -9,43 +9,40 @@ import java.util.Arrays;
 import java.util.List;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @NoArgsConstructor
 @Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private  Long id;
 
     private String username;
     private String password;
     private int active;
     private String roles;
-    private String permission;
+    private String permissions;
 
-
-    public User(String username, String password,String roles, String permission) {
+    public User(String username, String password,String roles, String permissions) {
         this.username = username;
         this.password = password;
+        this.active = 1;
         this.roles = roles;
-        this.permission = permission;
+        this.permissions = permissions;
     }
 
     public List<String> getRoleList(){
-
         if(this.roles.length()>0){
-
             return Arrays.asList(this.roles.split(","));
         }
         return new ArrayList<>();
     }
 
-    public List<String> getPermission(){
-
-        if(this.permission.length()>0){
-
-            return Arrays.asList(this.permission.split(","));
+    public List<String> getPermissionList(){
+        if(this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split(","));
         }
         return new ArrayList<>();
+
     }
 }
