c0e60d6e02df7c6c8acf6dee131e2d05b1da7d69
Mehmet <mhmtsytrk@gmail.com>
2021-01-17 22:25:47 +0300

    Security-HttpBasic-Database

---
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/security/UserPrincipalDetailsService.java
@@ -0,0 +1,23 @@
+package com.cybertek.security;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserPrincipalDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user=userRepository.findByUsername(s);
+
+        UserPrincipal userPrincipal=new UserPrincipal(user);
+        return userPrincipal;
+    }
+}
