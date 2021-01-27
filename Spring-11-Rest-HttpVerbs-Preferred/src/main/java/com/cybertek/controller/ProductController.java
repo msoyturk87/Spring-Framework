8c59442be36cc39cd90375ac25499c1d33c4ec0a
Mehmet <mhmtsytrk@gmail.com>
2021-01-27 03:15:01 +0300

    Security-Rest-HttpVerbs-Preferred

--- Spring-11-Rest-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/products")
 public class ProductController {
 
     private ProductService productService;
@@ -17,29 +18,29 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.GET)
-    public @ResponseBody Product getProduct(@PathVariable("id") long id){
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public  Product getProduct(@PathVariable("id") long id){
         return productService.getProduct(id);
     }
 
-    @RequestMapping(value = "/products")
-    public @ResponseBody List<Product> getProducts(){
+    @RequestMapping
+    public  List<Product> getProducts(){
         return productService.getProducts();
     }
 
 
-    @RequestMapping(value = "/products",method = RequestMethod.POST)
-    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
+    @RequestMapping(method = RequestMethod.POST)
+    public  List<Product> createProduct(@RequestBody Product product){
         return productService.createProduct(product);
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
-    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public  List<Product> deleteProduct(@PathVariable("id") long id){
         return productService.delete(id);
     }
 
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
-    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public  List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
         return productService.updateProduct(id, product);
     }
 
