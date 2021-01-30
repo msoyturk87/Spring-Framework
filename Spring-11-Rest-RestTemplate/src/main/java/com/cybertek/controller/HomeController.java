a71bcb10535fa7216823afd866e94e2031344ce9
Mehmet <mhmtsytrk@gmail.com>
2021-01-30 20:48:12 +0300

    Security-Rest-RestTemplate

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,20 +1,19 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
-
 
 @RestController
 public class HomeController {
 
-    final String URI="https://jsonplaceholder.typicode.com/users";
-
+    final String URI = "https://jsonplaceholder.typicode.com/users";
 
     private RestTemplate restTemplate;
 
@@ -24,20 +23,32 @@ public class HomeController {
 
     @GetMapping
     public User[] readAllUsers(){
-
-        ResponseEntity<User[]> responseEntity=restTemplate.getForEntity(URI,User[].class);
-
-
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI,User[].class);
         return responseEntity.getBody();
     }
 
-    @GetMapping(value ="/{id}")
+    @GetMapping(value = "/{id}")
     public Object readUser(@PathVariable("id") Integer id){
+        String URL = URI + "/{id}";
+        return restTemplate.getForObject(URL,Object.class,id);
+    }
 
-        String URL=URI+"/{id}";
-        return restTemplate.getForObject(URL, Object.class,id);
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostsFromDummyApi(){
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","lTE5abbDxdjGplutvTuc");
 
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/api/user?limit=10", HttpMethod.GET,entity,Object.class);
+
+        return response;
 
     }
+
+
+
+
 }
