ee9e4f783150d8a6dc6d9e486c9ab34bc2922c45
Mehmet <mhmtsytrk@gmail.com>
2021-01-30 20:17:58 +0300

    Security-Rest-RestTemplate

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/ResttemplateApplication.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/ResttemplateApplication.java
@@ -2,6 +2,12 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class ResttemplateApplication {
@@ -9,5 +15,14 @@ public class ResttemplateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResttemplateApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder
+				.setConnectTimeout(Duration.ofMillis(3000))
+				.setReadTimeout(Duration.ofMillis(2000))
+				.build();
+	}
+
 
 }
