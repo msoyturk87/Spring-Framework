051e82dcfea501314136da24ac0ffdd3bc1b6017
Mehmet <mhmtsytrk@gmail.com>
2021-02-05 23:56:18 +0300

    WebClient

--- Spring-11-Rest-Jackson-CinemaApp/src/main/java/com/cybertek/repository/GenreRepository.java
+++ Spring-11-Rest-Jackson-CinemaApp/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -7,20 +7,25 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
 
     // ------------------- JPQL QUERIES ------------------- //
+//
+//    //Write a JPQL query that return all genres
+//    @Query("SELECT g FROM Genre g")
+//    List<Genre> fetchAllJPQL();
+//
+//    // ------------------- Native QUERIES ------------------- //
+//
+//    //Write a native query that returns genres by containing name
+//    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
+//    List<Genre> retrieveByName(String name);
 
-    //Write a JPQL query that return all genres
-    @Query("SELECT g FROM Genre g")
-    List<Genre> fetchAllJPQL();
-
-    // ------------------- Native QUERIES ------------------- //
-
-    //Write a native query that returns genres by containing name
-    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
-    List<Genre> retrieveByName(String name);
+    @Query(value = "SELECT count(*) from genre g JOIN movie_genre_rel mgr on g.id = mgr.genre_id WHERE g.id = ?1", nativeQuery = true)
+    Integer countGenresNativeQuery(Long id);
 
 
 }
+
