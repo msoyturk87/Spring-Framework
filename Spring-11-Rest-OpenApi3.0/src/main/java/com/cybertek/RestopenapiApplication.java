92689c35f4c81c96f8166775bfe3c70ad893448c
Mehmet <mhmtsytrk@gmail.com>
2021-02-06 14:04:05 +0300

    Rest OpenApi

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/RestopenapiApplication.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/RestopenapiApplication.java
@@ -1,5 +1,6 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class RestopenapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestopenapiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenApi(){
+
+		return new OpenAPI()
+				.info(new Info().title("Cinema Application")
+						.version("v1")
+						.description("Selling online cinema tickets"));
 	}
 
 }
