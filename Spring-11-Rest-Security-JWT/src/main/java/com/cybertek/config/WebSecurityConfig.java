e50e0682d226b43c1fe4a38e372fae6cca341526
Mehmet <mhmtsytrk@gmail.com>
2021-02-06 20:06:06 +0300

    Rest-Security-JWT

---
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.cybertek.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+        public AuthenticationManager authenticationManagerBean() throws Exception{
+
+            return super.authenticationManagerBean();
+        }
+
+        protected  void configure(HttpSecurity http) throws Exception{
+
+            http
+                    .csrf()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers("/authenticate")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+                    // they are disable this one from real
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
